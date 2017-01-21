787d0b53c637a40d89cfaa489d6c7da7bd19cdfe
Moritz Kornher <mail@moritzkornher.de>
2017-01-21 13:28:13 +1300

    Start with a random coat-of-arms in the slider

--- assets/js/frogsystem.js
+++ assets/js/frogsystem.js
@@ -1,27 +1,26 @@
-$(function() {
+$(function () {
     $('.jcarousel')
         .jcarousel({
             wrap: 'circular',
             transitions: true,
             animation: {
                 duration: 1000,
-                easing:   'swing'
+                easing: 'swing'
             }
         })
-        .jcarousel('scroll', -1, false)
+        .jcarousel('scroll', '+=' + Math.floor(Math.random() * $('.jcarousel ul li').length), false)
         .jcarouselAutoscroll({
             interval: 2000,
             target: '+=1',
             autostart: true
         })
-    ;
-    $('.jcarousel').hover(
-        function() {
-            $(this).jcarouselAutoscroll('stop');
-        }, function() {
-            $(this).jcarouselAutoscroll('start');
-        }
-    );
+        .hover(
+            function () {
+                $(this).jcarouselAutoscroll('stop');
+            }, function () {
+                $(this).jcarouselAutoscroll('start');
+            }
+        );
     $('.jcarousel-prev').jcarouselControl({
         target: '-=2'
     });
