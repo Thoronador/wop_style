33401f8eafc775409280787f4d15637b1d78e267
Moritz Kornher <mail@moritzkornher.de>
2017-01-21 13:28:13 +1300

    Start with a random coat-of-arms in the slider

--- www/styles/wop/main.js
+++ www/styles/wop/main.js
@@ -159,30 +159,29 @@ return d||(f=$b[b],$b[b]=e,e=null!=c(a,b,d)?b.toLowerCase():null,$b[b]=f),e}});v
 
 }(jQuery);
 
-$(function() {
+$(function () {
     $('.jcarousel')
         .jcarousel({
             wrap: 'circular',
             transitions: true,
             animation: {
                 duration: 1000,
-                easing:   'swing'
+                easing: 'swing'
             }
         })
-        .jcarousel('scroll', -1, false)
+        .jcarousel('scroll', '+=' + Math.floor(Math.random() * $('.jcarousel ul li').length), false)
         .jcarouselAutoscroll({
             interval: 2000,
             target: '+=1',
             autostart: true
         })
-    ;
-    $('.jcarousel').hover(
-        function() {
-            $(this).jcarouselAutoscroll('stop');
-        }, function() {
-            $(this).jcarouselAutoscroll('start');
-        }
-    );
+        .hover(
+            function () {
+                $(this).jcarouselAutoscroll('stop');
+            }, function () {
+                $(this).jcarouselAutoscroll('start');
+            }
+        );
     $('.jcarousel-prev').jcarouselControl({
         target: '-=2'
     });
