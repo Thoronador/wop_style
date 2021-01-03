1e7bd90794e314794486b235432e02b3a571370a
Moritz Kornher <mail@moritzkornher.de>
2021-01-03 18:30:45 +0100

    chore(style): upgrade to jqyer 2.2.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
         concat: {
             dist: {
                 src: [
-                    'assets/js/jquery-2.1.0.min.js',
+                    'assets/js/jquery-2.2.4.min.js',
                     'assets/js/jquery.jcarousel.min.js',
                     'assets/js/bootstrap/dropdown.js',
                     'assets/js/js.cookie.js',
